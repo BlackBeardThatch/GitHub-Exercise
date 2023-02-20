6e086b64fbf421d72be84661b221734b21b41e6c
Jason Hung <cp3jason@gmail.com>
2023-02-20 22:06:19 +0800

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by 洪正倫 on 2023/2/20.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
